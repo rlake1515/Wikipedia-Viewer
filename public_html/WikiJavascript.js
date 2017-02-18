93edc9d9ded13894fa222053e261b79c3b62c82b
Robin Lake <mister.lake@gmail.com>
2017-02-17 17:17:47 -0700

    Basic features working, no aesthetics added

--- public_html/WikiJavascript.js
+++ public_html/WikiJavascript.js
@@ -1,7 +1,46 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
+
+$(document).ready(function(){
+  // click search button to activate getWikiAPI
+  $("#searchButton").click(function(){
+       $("#pageOutput").empty();
+       console.log($("#searchText").val()); 
+       getWikiAPI($("#searchText").val());
+    });    
+  // press enter to activate getWikiAPI
+  $("#searchText").keypress(function(e){
+     if (e.which === 13){
+       $("#searchButton").click();  
+     } 
+  });  
+  // click random button to activate getRandomPage  
+  $("#randomButton").click(function(){
+       getRandomPage(); 
+    });
+});
+// global search variable
+searchText = "bees";
+
+// AJAX call for Wikipedia search
+function getWikiAPI(searchText){
+    if(searchText !== ""){        
+    let ourURL =`https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=${searchText}&callback=?&origin=*`;
+   console.log(searchText);
+    $.ajax(ourURL,{
+        dataType: "json",
+        success: function(data){
+           console.log(data);
+           for(let i=0; i<data[1].length; i++){
+               $('#pageOutput').append("<li><a href = "+ data[3][i]+">"+ data[1][i]+"</a>","<br>", data[2][i], "</li>");
+       }
+       $("#searchText").val("");
+       }
+    });    
+}
+}
+
+// AJAX call for random search
+function getRandomPage(){
+      window.open("https://en.wikipedia.org/wiki/Special:Random");
+};
